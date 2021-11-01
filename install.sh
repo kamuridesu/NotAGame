2d955733a82419073c65ee76a2f0bff49cd88192
Kamuri Amorim <myk.gata14@gmail.com>
2021-11-01 08:26:37 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
-if which apt; then
+if which apt 2>&1 > /dev/null; then
 	sudo apt update
-	sudo apt install python python3
-	sudo apt install python3-pip
-elif which pacman; then
-	sudo pacman -Sy python python3-pip
+	sudo apt install python python3 -y
+	sudo apt install python3-pip -y
+elif which pacman 2>&1 > /dev/null; then
+	sudo pacman --noconfirm -Sy python python3-pip
 fi
