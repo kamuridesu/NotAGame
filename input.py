302d58541c5caf6701b03939af468b1173f3fb3d
Kamuri Amorim <myk.gata14@gmail.com>
2022-07-01 12:52:02 -0300

    Update input.py

--- input.py
+++ input.py
@@ -2,25 +2,35 @@ import sys
 import termios
 import atexit
 from select import select
+WINDOWS = False
+try:
+    import msvcrt
+    windows = True
+except ImportError:
+    WINDOWS=False
 
 
 class Keyboard:
     def __init__(self) -> None:
-        self.fd: int = sys.stdin.fileno()
-        self.new_term: list = termios.tcgetattr(self.fd)
-        self.old_term: list = termios.tcgetattr(self.fd)
+        if not WINDOWS:
+            self.fd: int = sys.stdin.fileno()
+            self.new_term: list = termios.tcgetattr(self.fd)
+            self.old_term: list = termios.tcgetattr(self.fd)
 
-        self.new_term[3] = (self.new_term[3] & ~termios.ICANON & ~termios.ECHO)
-        termios.tcsetattr(self.fd, termios.TCSAFLUSH, self.new_term)
+            self.new_term[3] = (self.new_term[3] & ~termios.ICANON & ~termios.ECHO)
+            termios.tcsetattr(self.fd, termios.TCSAFLUSH, self.new_term)
 
-        atexit.register(self.setNormalTerm)
+            atexit.register(self.setNormalTerm)
     
     def setNormalTerm(self) -> None:
         termios.tcsetattr(self.fd, termios.TCSAFLUSH, self.old_term)
     
     def getch(self) -> str:
-        key = sys.stdin.read(1)
-        return key
+        if not WINDOWS:
+            key = sys.stdin.read(1)
+            return key
+        else:
+            return msvcrt.getch()
 
     def getArrow(self):
         inp = sys.stdin.read(3)[2]
@@ -30,7 +40,10 @@ class Keyboard:
             return vals.index(ord(inp))
     
     def hit(self) -> tuple:
-        return select([sys.stdin], [], [], 0)[0] != []
+        if not WINDOWS:
+            return select([sys.stdin], [], [], 0)[0] != []
+        else:
+            return msvcrt.kbhit()
 
 
 def save(content) -> None:
